d6ddef27b7d850e80be5aa9351c21c4984c13d7c
Ncia89 <nicholas.ciarelli@gmail.com>
2019-10-23 15:01:49 -0400

    Added the Contact class which only has a constructor method to get its four properties.

---
+++ script.js
@@ -0,0 +1,12 @@
+// Address Book Lab
+// Required: AddressBook Class, Contact class, print function
+
+// Contact Class: A class for storing the name, email, phone, and relations of a contact in AddressBook
+class Contact {
+  constructor(name, email, phone, relation) {
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.relation = relation;
+  }
+}
