7b597c046b34f3b51aa4357199b336746f29aff6
Ncia89 <nicholas.ciarelli@gmail.com>
2019-10-23 15:14:23 -0400

    Added the AddressBook Class. This includes an array of contacts and methods to add and delete single instances of the Contact class from the contacts array.

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ class Contact {
     this.relation = relation;
   }
 }
+
+// AddressBook Class: A class for storing and mainpulating an array of contacts of the Contact class
+class AddressBook {
+  constructor() {
+    this.contacts = [];
+  }
+
+  // Adds a new Contact to the contacts array of AddressBook
+  add(name, email, phone, relation) {
+    let newContact = new Contact(name, email, phone, relation);
+    this.contacts.push(newContact);
+  }
+
+  // Removes a contact located at index in the contacts array.
+  deleteAt(index) {
+    this.contacts.splice(index, 1);
+  }
+}
