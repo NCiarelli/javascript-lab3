06c9389762c4324c9f27b2547c0ec6bd3b29b208
Ncia89 <nicholas.ciarelli@gmail.com>
2019-10-23 20:46:04 -0400

    Added methods and functions for the extended challenge. Untested yet.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 // Address Book Lab
 // Required: AddressBook Class, Contact class, print function
 
@@ -12,20 +13,75 @@ class Contact {
 }
 
 // AddressBook Class: A class for storing and mainpulating an array of contacts of the Contact class
+// Extended Challenge functions: filterByName, filterByRelation, clear, edit, listNames, deleteByName
 class AddressBook {
   constructor() {
     this.contacts = [];
   }
 
-  // Adds a new Contact to the contacts array of AddressBook
+  // add: Adds a new Contact to the contacts array of AddressBook
   add(name, email, phone, relation) {
     let newContact = new Contact(name, email, phone, relation);
     this.contacts.push(newContact);
   }
 
-  // Removes a contact located at index in the contacts array.
+  // deleteAt: Removes a contact located at index in the contacts array.
   deleteAt(index) {
     this.contacts.splice(index, 1);
+  }
+
+  // filterByName: Returns the first contact with the given name
+  filterByName(name) {
+    for (const contact of this.contacts) {
+      if (contact.name === name) return contact;
+    }
+    return null;
+  }
+
+  // filterByRelation: Returns an array of all the contacts that have that relation specified.
+  filterByRelation(relation) {
+    filteredContacts = {};
+    for (const contact of this.contacts) {
+      if (contact.relation === relation) filteredContacts.push(contact);
+    }
+    return filteredContacts;
+  }
+
+  // clear: Remove all contacts
+  clear() {
+    // this.conatacts = {};  // Does this create floating memory packets that take up space or do JavaScript handlers automatically garbage collect this?
+    this.contacts.splice(0, this.contacts.length);
+  }
+
+  // edit: Has five parameters: oldName, name, email, phone, relation. Replace the old contact with the given oldName, with the new values.
+  edit(oldName, name, email, phone, relation) {
+    for (const contact of this.contacts) {
+      if (contact.name === oldName) {
+        contact.name = name;
+        contact.email = email;
+        contact.phone = phone;
+        contact.relation = relation;
+      }
+    }
+  }
+
+  // listNames - Return an array of just the names of all the contacts. This will be an array of strings.
+  listNames() {
+    nameArray = {};
+    for (const contact of this.contacts) {
+      nameArray.push(contact.name);
+    }
+    return nameArray;
+  }
+
+  // deleteByName - Has one parameter for name. Delete the contact with the given name.
+  deleteByName(name) {
+    for (let i = 0; i < this.contacts.length; i++) {
+      if (contacts[i].name === name) {
+        this.contacts.splice(i, 1);
+        return;
+      }
+    }
   }
 }
 
@@ -48,7 +104,20 @@ function print(addressBookToPrint) {
   }
 }
 
+function printNames(addressBookToPrint) {
+  if (addressBookToPrint.contacts.length === 0) {
+    console.log("The addressbook is empty.");
+    return;
+  } else {
+    console.log("These are the names in this addressbook:");
+    for (const contact of addressBookToPrint.contacts) {
+      console.log(contact.name);
+    }
+  }
+}
+
 let addressBook = new AddressBook();
+print(addressBook); // Test empty book check
 addressBook.add("Max", "max.ciarelli@gmail.com", "313-333-6666", "Father");
 addressBook.add("Dot", "dot.ciarelli@gmail.com", "313-555-7777", "Mother");
 addressBook.add("Lady", "lady.ciarelli@gmail.com", "313-777-0000", "Sister");
@@ -59,3 +128,5 @@ print(addressBook);
 addressBook.deleteAt(2);
 
 print(addressBook);
+
+printNames(addressBook);
